fde1f0e3f5c4b35e0a2cebddf274d57c9e84ef73
Nicholas Yong <nicholas5538@gmail.com>
2022-12-20 15:32:41 +0800

    Modifying default colors for text-color
    Modifying both black and pink color rgb values, with the latter from value 300 - 900

--- frontend-voucher/tailwind.config.cjs
+++ frontend-voucher/tailwind.config.cjs
@@ -10,6 +10,13 @@ module.exports = {
     },
     colors: {
       black: "hsl(0, 0%, 20%)",
+      pink: {
+        300: 'hsl(334, 24%, 45%)',
+        400: 'hsl(334, 79%, 43%)',
+        500: 'hsl(334, 79%, 50%)',
+        800: 'hsl(334, 79%, 85%)',
+        900: 'hsl(334, 79%, 90%)',
+      }
     },
     fontFamily: {
       sans: 'Open Sans, Arial, sans-serif',
