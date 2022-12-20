ef99f5c2d811d086a3402d8a46972bb41c4c6bf7
Nicholas Yong <nicholas5538@gmail.com>
2022-12-20 14:48:29 +0800

    Install dependency vite-plugin-svgr
    Install plugin for SVG imports and customization of text-color, font-family and breakpoints on tailwind.config.cjs

--- frontend-voucher/tailwind.config.cjs
+++ frontend-voucher/tailwind.config.cjs
@@ -1,7 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
+    screens: {
+      'xs': '568px',
+      'sm': '640px',
+      'md': '768px',
+      'lg': '992px', 
+    },
+    colors: {
+      black: "hsl(0, 0%, 20%)",
+    },
+    fontFamily: {
+      sans: 'Open Sans, Arial, sans-serif',
+    },
     extend: {},
   },
   plugins: [],
