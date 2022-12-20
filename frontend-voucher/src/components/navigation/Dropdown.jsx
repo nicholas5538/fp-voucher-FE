850a8839cdfad3af19a647780e1004ff82016588
Nicholas Yong <nicholas5538@gmail.com>
2022-12-20 19:57:53 +0800

    Implementation of Navbar and its dropdown menu
    The following features have been added:
    1. Navbar without breakpoints
    2. Dropdown menu and its menu items
    3. Event Listener for both button and document body
    4. Using @layer directive to set a custom style for dropdown li tags

---
+++ frontend-voucher/src/components/navigation/Dropdown.jsx
@@ -0,0 +1,38 @@
+import { Link } from "react-router-dom";
+import { menuItems } from "./MenuItems";
+
+{ /* Edit Link 'to' attribute in MenuItems.jsx */ }
+
+function Dropdown() {
+
+  const liElements = menuItems.map((item) => {
+    return (
+      <li key={item.id} className="li-dropdown">
+        {item.id === 2 ? (
+          <Link
+            key={item.id}
+            className="grid gap-x-1 grid-cols-3 items-center"
+            to={item.url}
+          >
+            <p className="col-span-2">Become a pandapro</p>
+            <span className="col-span-1">{item.element}</span>
+          </Link>
+        ) : (
+          <Link key={item.id} to={item.url}>
+            {item.element}
+          </Link>
+        )}
+      </li>
+    );
+  });
+
+  return (
+    <ul
+      className="absolute z-10 py-2 shadow-md text-black text-start bg-white border-[0.02em] border-gray-200 top-[65px] w-[273px]"
+    >
+      {liElements}
+    </ul>
+  );
+}
+
+export default Dropdown;
--- frontend-voucher/src/components/navigation/MainNavigation.jsx
+++ frontend-voucher/src/components/navigation/MainNavigation.jsx
@@ -1,9 +1,60 @@
+import { useRef, useState } from "react";
 import { NavLink } from "react-router-dom";
-
-import classes from "./MainNavigation.module.css";
+import { IconContext } from "react-icons";
+import { ReactComponent as Logo } from "../../assets/logo.svg";
+import { ReactComponent as Brand } from "../../assets/brand.svg";
+import { ReactComponent as Profile } from "../../assets/profile.svg";
+import { ReactComponent as Favourite } from "../../assets/favourite.svg";
+import { ReactComponent as Cart } from "../../assets/cart.svg";
+import { FaAngleDown } from "react-icons/fa";
+import Dropdown from "./Dropdown";
+import useOutsideAlerter from "./useOutSideAlerter";
 
 function MainNavigation() {
-  return <></>;
+  const [open, setOpen] = useState(false);
+  const dropdownRef = useRef();
+  useOutsideAlerter(dropdownRef, setOpen)
+
+  return (
+    <nav className="relative z-0 flex place-content-between items-center shadow-lg h-16">
+      <div ref={dropdownRef}>
+        <button
+          className="flex items-center space-x-2 px-4 h-16"
+          onClick={() => setOpen(prevState => !prevState)}
+        >
+          <Profile />
+          {/* Replace hardcoded name with Authentication */}
+          <span className="hidden text-black text-xs text-ellipsis text-center font-bold font-sans">
+            ANDY
+          </span>
+          <IconContext.Provider
+            value={{ color: "rgb(226, 27, 112)", className: "hidden" }}
+          >
+            <FaAngleDown />
+          </IconContext.Provider>
+        </button>
+        {open ? <Dropdown setOpen={setOpen}/> : null}
+      </div>
+      <div className="px-6 w-3/4 h-16">
+        <div className="mx-auto my-4 w-1/3 h-1/2">
+          <NavLink to="/" className="flex place-content-center" end>
+            <Logo className="hidden" />
+            <Brand />
+          </NavLink>
+        </div>
+      </div>
+      <div className="px-2">
+        <NavLink to="/favourites" end>
+          <Favourite />
+        </NavLink>
+      </div>
+      <div className="px-4">
+        <NavLink to="/cart" end>
+          <Cart />
+        </NavLink>
+      </div>
+    </nav>
+  );
 }
 
 export default MainNavigation;
