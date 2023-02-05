71764f277683ee3b8d02249745468465d484c870
Rui_Yang Chua <chuaruiyang@gmail.com>
2023-02-05 21:46:22 +0800

    Edit breakpoint for Home component and add sticky attribute to MainNavigation component

--- frontend-voucher/src/components/navigation/MainNavigation.tsx
+++ frontend-voucher/src/components/navigation/MainNavigation.tsx
@@ -16,7 +16,7 @@ const MainNavigation = () => {
   useOutsideAlerter(dropdownRef, setOpen);
 
   return (
-    <nav className='shadow-lg w-full'>
+    <nav className='shadow-lg w-full sticky top-0'>
       <div className='flex justify-between items-center mx-auto w-full max-w-7xl h-16'>
         <div
           className='relative lg:order-2 lg:border-x lg:border-gray-200'
