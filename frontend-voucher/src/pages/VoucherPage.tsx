7d4c0e8edfe16f45a176e7ed8154c62f78e3a75d
nicholas5538 <nicholas5538@gmail.com>
2023-03-05 14:45:30 +0800

    Add style-components

--- frontend-voucher/src/pages/VoucherPage.tsx
+++ frontend-voucher/src/pages/VoucherPage.tsx
@@ -1,7 +1,11 @@
-import React from 'react';
+import Paper from '@mui/material/Paper';
 
 const VoucherPage = () => {
-  return <section className='mx-auto max-w-7xl'></section>;
+  return (
+    <section className='mx-auto max-w-7xl'>
+      <Paper elevation={2}></Paper>
+    </section>
+  );
 };
 
 export default VoucherPage;
