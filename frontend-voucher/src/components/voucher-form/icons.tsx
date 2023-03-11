c4f6dfbd2df94173c16f7fef05625dd1dfa88d51
nicholas5538 <nicholas5538@gmail.com>
2023-03-11 22:56:29 +0800

    Migrate form to components folder

---
+++ frontend-voucher/src/components/voucher-form/icons.tsx
@@ -0,0 +1,21 @@
+import AttachMoneyOutlinedIcon from '@mui/icons-material/AttachMoneyOutlined';
+import CancelIcon from '@mui/icons-material/Cancel';
+import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
+import DescriptionOutlinedIcon from '@mui/icons-material/DescriptionOutlined';
+import DiscountOutlinedIcon from '@mui/icons-material/DiscountOutlined';
+import PercentOutlinedIcon from '@mui/icons-material/PercentOutlined';
+import RestartAltIcon from '@mui/icons-material/RestartAlt';
+import SendIcon from '@mui/icons-material/Send';
+
+const icons = {
+  attachMoney: <AttachMoneyOutlinedIcon />,
+  cancel: <CancelIcon />,
+  delete: <DeleteForeverIcon />,
+  desc: <DescriptionOutlinedIcon />,
+  discount: <DiscountOutlinedIcon />,
+  percent: <PercentOutlinedIcon />,
+  reset: <RestartAltIcon />,
+  send: <SendIcon />,
+};
+
+export default icons;
