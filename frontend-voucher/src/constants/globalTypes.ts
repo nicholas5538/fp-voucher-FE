2df2ad03582aa884d6b22071ce836d827f446333
nicholas5538 <nicholas5538@gmail.com>
2023-03-11 22:53:07 +0800

    Create mock api for GET

--- frontend-voucher/src/constants/globalTypes.ts
+++ frontend-voucher/src/constants/globalTypes.ts
@@ -6,8 +6,16 @@ export type voucherFormValues = {
   category: string;
   description: string;
   discount: number;
+  id: string;
   minSpending: number;
   promoCode: string;
   startDate: Dayjs;
   expiryDate: Dayjs;
+};
+
+export type dataType = {
+  page: number;
+  total: number;
+  total_pages: number;
+  vouchers: voucherFormValues[];
 };
