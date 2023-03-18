8f9f26b842c40dd56f8c2286cca71d547dc800e7
Nicholas <nicholas5538@gmail.com>
2023-03-18 15:00:36 +0800

    Create footer for web app (#30)

---
+++ frontend-voucher/src/components/footer.tsx
@@ -0,0 +1,71 @@
+import clsx from 'clsx';
+import { Link } from 'react-router-dom';
+import { ReactComponent as Logo } from '..//assets/logo.svg';
+import { ReactComponent as Brand } from '../assets/brand.svg';
+import { useUserContext } from '../hooks/useUserContext';
+
+const Footer = () => {
+  const { token } = useUserContext();
+  const sitemap = [
+    {
+      url: '/vouchers',
+      label: 'View vouchers',
+    },
+    {
+      url: '/vouchers/create',
+      label: 'Create voucher',
+    },
+    {
+      url: '/vouchers',
+      label: 'Update voucher',
+    },
+    {
+      url: '/vouchers',
+      label: 'Delete voucher',
+    },
+    {
+      url: '/carts',
+      label: 'Cart',
+    },
+  ];
+
+  const siteMapLinks = sitemap.map(({ url, label }, index) => {
+    return (
+      <Link
+        key={index.toString()}
+        to={url}
+        className={clsx('px-4 pb-4 no-underline md:pb-0', {
+          'm-0 border-0 border-b border-solid border-black md:border-b-0 md:border-r':
+            index !== sitemap.length - 1,
+          'md:pl-0': index === 0,
+        })}
+      >
+        <p className='text-sm font-medium text-black'>{label}</p>
+      </Link>
+    );
+  });
+
+  return (
+    <footer className='border-0 border-t-2 border-solid border-pink-500 py-8 px-4'>
+      <div className='mx-auto flex max-w-7xl flex-col md:items-start xl:px-4'>
+        {token ? (
+          <>
+            <h3 className='mb-4 text-center text-lg'>Overview</h3>
+            <div className='mb-4 flex flex-col items-center justify-center space-y-4 px-4 md:flex-row md:space-y-0 md:px-0'>
+              {siteMapLinks}
+            </div>
+          </>
+        ) : null}
+        <div className='flex flex-col items-center md:items-start'>
+          <Link to='/' className='mb-4 md:flex md:space-x-2'>
+            <Logo className='hidden md:block' />
+            <Brand />
+          </Link>
+          <p className='text-sm'>&copy; 2023 foodpanda. All Rights Reserved.</p>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
