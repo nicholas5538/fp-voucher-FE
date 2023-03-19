648e5f8a8db162975be2fa2f94f4c6d50ddcce75
nicholas5538 <nicholas5538@gmail.com>
2023-03-19 17:07:37 +0800

    Add resizing animation for table and form

--- frontend-voucher/src/components/alert.tsx
+++ frontend-voucher/src/components/alert.tsx
@@ -1,11 +1,30 @@
 import Alert, { type AlertProps } from '@mui/material/Alert';
+import { AnimatePresence, motion } from 'framer-motion';
 
 type AlertComponentProps = AlertProps & {
   text: string;
+  shouldRender: boolean;
 };
 
-const AlertComponent = ({ text, ...props }: AlertComponentProps) => {
-  return <Alert {...props}>{text}</Alert>;
+const AlertComponent = ({
+  text,
+  shouldRender,
+  ...props
+}: AlertComponentProps) => {
+  return (
+    <AnimatePresence>
+      {shouldRender && (
+        <motion.aside
+          className='w-full'
+          initial={{ opacity: 0, y: -20 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ type: 'spring', duration: 0.8, bounce: 0.4 }}
+        >
+          <Alert {...props}>{text}</Alert>
+        </motion.aside>
+      )}
+    </AnimatePresence>
+  );
 };
 
 export default AlertComponent;
--- frontend-voucher/src/components/voucher-form/MemoVoucherForm.tsx
+++ frontend-voucher/src/components/voucher-form/MemoVoucherForm.tsx
@@ -2,11 +2,12 @@ import { yupResolver } from '@hookform/resolvers/yup';
 import CheckCircleOutlineIcon from '@mui/icons-material/CheckCircleOutline';
 import ButtonGroup from '@mui/material/ButtonGroup';
 import Paper from '@mui/material/Paper';
-import clsx from 'clsx';
+import { motion } from 'framer-motion';
 import Lottie from 'lottie-react';
 import { memo, useEffect, useState } from 'react';
 import { SubmitHandler, useForm } from 'react-hook-form';
 import { useNavigate } from 'react-router-dom';
+import useMeasure from 'react-use-measure';
 import downArrow from '../../assets/down-arrow.json';
 import { actionLabels, categoryLabels } from '../../constants/form-labels';
 import voucherFormSchema from '../../constants/form-schema';
@@ -27,9 +28,10 @@ type VoucherFormProps = {
 
 const VoucherFormComponent = ({ defaultValues }: VoucherFormProps) => {
   document.title = `${defaultValues.action} Foodpanda Voucher`;
+  const navigate = useNavigate();
   const [openModal, setOpenModal] = useState(() => false);
   const [openDeleteModal, setDeleteModal] = useState(() => false);
-  const navigate = useNavigate();
+  const [ref, { height }] = useMeasure();
 
   const {
     control,
@@ -85,170 +87,170 @@ const VoucherFormComponent = ({ defaultValues }: VoucherFormProps) => {
     <>
       <Paper
         elevation={3}
-        className='mx-auto max-w-2xl rounded-lg py-4 lg:mx-0 xl:max-w-3xl'
+        className='mx-auto max-w-2xl rounded-lg pt-4 pb-12 lg:mx-0 xl:max-w-3xl'
       >
-        <div
-          className={clsx(
-            'mb-4 border-0 border-b border-solid border-gray-700 pb-2',
-            { 'mb-0': isSubmitSuccessful },
-          )}
-        >
+        <div className='border-0 border-b border-solid border-gray-700 pb-2'>
           <h2 className='ml-3 font-mont text-2xl font-semibold tracking-wider text-gray-700 xl:text-3xl'>
             {watchAction} voucher
           </h2>
         </div>
-        {isSubmitSuccessful && (
-          <AlertComponent
-            className='mb-4 px-2'
-            iconMapping={{
-              success: <CheckCircleOutlineIcon fontSize='inherit' />,
-            }}
-            text={`The voucher has been successfully ${watchAction.toLowerCase()}d! You'll be redirected shortly.`}
-          />
-        )}
-        <form onSubmit={handleSubmit(onSubmit)} className='px-3'>
-          {watchAction !== 'Create' && (
-            <RadioInputs
-              control={control}
-              label='Select an action'
-              labelsObject={actionLabels}
-              name='action'
+        <motion.div animate={{ height }} transition={{ duration: 0.3 }}>
+          <div ref={ref}>
+            <AlertComponent
+              className='mb-4 px-2'
+              iconMapping={{
+                success: <CheckCircleOutlineIcon fontSize='inherit' />,
+              }}
+              shouldRender={isSubmitSuccessful}
+              text={`The voucher has been successfully ${watchAction.toLowerCase()}d! You'll be redirected shortly.`}
             />
-          )}
-          <RadioInputs
-            control={control}
-            disabled={disabledWatchAction}
-            label='Select a category'
-            labelsObject={categoryLabels}
-            name='category'
-          />
-          <div className='mb-6 grid grid-cols-1 space-y-6 md:grid-cols-3 md:space-y-0 md:space-x-4'>
-            <TextFieldComponent
-              className='md:col-span-2'
-              control={control}
-              disabled={disabledWatchAction}
-              error={errors.description === undefined ? false : true}
-              helperText={errors.description?.message}
-              icon={icons.desc}
-              label='Description'
-              multiline
-              maxRows={2}
-              name='description'
-              placeholder='5% off pick-up on Pizza Hut'
-              type='text'
-            />
-            <TextFieldComponent
-              className='col-span-1'
-              control={control}
-              disabled={disabledWatchAction}
-              error={errors.promoCode === undefined ? false : true}
-              helperText={errors.promoCode?.message}
-              icon={icons.discount}
-              label='Promo code'
-              name='promoCode'
-              placeholder='PIZZAHUT5'
-              type='text'
-            />
-          </div>
-          <div className='mb-6 grid grid-cols-1 space-y-6 md:grid-cols-2 md:space-y-0 md:space-x-4'>
-            <TextFieldComponent
-              className='col-span-1'
-              control={control}
-              disabled={disabledWatchAction}
-              error={errors.minSpending === undefined ? false : true}
-              helperText={errors.minSpending?.message}
-              icon={icons.attachMoney}
-              label='Minimum spending'
-              name='minSpending'
-              type='number'
-            />
-            <TextFieldComponent
-              className='col-span-1'
-              control={control}
-              disabled={disabledWatchAction}
-              error={errors.discount === undefined ? false : true}
-              helperText={errors.discount?.message}
-              icon={icons.percent}
-              label='Discount'
-              name='discount'
-              type='number'
-            />
-          </div>
-          <div className='mb-6 flex flex-col items-start space-y-6 md:flex-row md:items-center md:justify-between md:space-y-0 md:space-x-5'>
-            <DateSelector
-              action={watchAction}
-              control={control}
-              disabled={disabledWatchAction}
-              disablePast={false}
-              title='Select start date'
-              name='startDate'
-            />
-            <DateSelector
-              action={watchAction}
-              control={control}
-              disabled={disabledWatchAction}
-              disablePast={true}
-              title='Select expiry date'
-              name='expiryDate'
-            />
-          </div>
-          <ButtonGroup
-            color='secondary'
-            aria-label='contained secondary button group'
-          >
-            {disabledWatchAction ? (
-              <>
+            <form onSubmit={handleSubmit(onSubmit)} className='mt-4 px-3'>
+              {watchAction !== 'Create' && (
+                <RadioInputs
+                  control={control}
+                  label='Select an action'
+                  labelsObject={actionLabels}
+                  name='action'
+                />
+              )}
+              <RadioInputs
+                control={control}
+                disabled={disabledWatchAction}
+                label='Select a category'
+                labelsObject={categoryLabels}
+                name='category'
+              />
+              <div className='mb-6 grid grid-cols-1 space-y-6 md:grid-cols-3 md:space-y-0 md:space-x-4'>
+                <TextFieldComponent
+                  className='md:col-span-2'
+                  control={control}
+                  disabled={disabledWatchAction}
+                  error={errors.description === undefined ? false : true}
+                  helperText={errors.description?.message}
+                  icon={icons.desc}
+                  label='Description'
+                  multiline
+                  maxRows={2}
+                  name='description'
+                  placeholder='5% off pick-up on Pizza Hut'
+                  type='text'
+                />
+                <TextFieldComponent
+                  className='col-span-1'
+                  control={control}
+                  disabled={disabledWatchAction}
+                  error={errors.promoCode === undefined ? false : true}
+                  helperText={errors.promoCode?.message}
+                  icon={icons.discount}
+                  label='Promo code'
+                  name='promoCode'
+                  placeholder='PIZZAHUT5'
+                  type='text'
+                />
+              </div>
+              <div className='mb-6 grid grid-cols-1 space-y-6 md:grid-cols-2 md:space-y-0 md:space-x-4'>
+                <TextFieldComponent
+                  className='col-span-1'
+                  control={control}
+                  disabled={disabledWatchAction}
+                  error={errors.minSpending === undefined ? false : true}
+                  helperText={errors.minSpending?.message}
+                  icon={icons.attachMoney}
+                  label='Minimum spending'
+                  name='minSpending'
+                  type='number'
+                />
+                <TextFieldComponent
+                  className='col-span-1'
+                  control={control}
+                  disabled={disabledWatchAction}
+                  error={errors.discount === undefined ? false : true}
+                  helperText={errors.discount?.message}
+                  icon={icons.percent}
+                  label='Discount'
+                  name='discount'
+                  type='number'
+                />
+              </div>
+              <div className='mb-6 flex flex-col items-start space-y-6 md:flex-row md:items-center md:justify-between md:space-y-0 md:space-x-5'>
+                <DateSelector
+                  action={watchAction}
+                  control={control}
+                  disabled={disabledWatchAction}
+                  disablePast={false}
+                  title='Select start date'
+                  name='startDate'
+                />
+                <DateSelector
+                  action={watchAction}
+                  control={control}
+                  disabled={disabledWatchAction}
+                  disablePast={true}
+                  title='Select expiry date'
+                  name='expiryDate'
+                />
+              </div>
+              <ButtonGroup
+                color='secondary'
+                aria-label='contained secondary button group'
+              >
+                {disabledWatchAction ? (
+                  <>
+                    <ButtonComponent
+                      isLoadingButton={false}
+                      label='Delete'
+                      onClick={() => setDeleteModal(true)}
+                      startIcon={icons.delete}
+                    />
+                    <ModalComponent
+                      modalTitle='Are you sure you want to delete the voucher?'
+                      modalDesc='Warning, all actions are irreversible.'
+                      // Note to myself: Need to change the line below to handle DELETE API
+                      clickHandler={() =>
+                        setDeleteModal((prevState) => !prevState)
+                      }
+                      openModal={openDeleteModal}
+                      setOpenModal={setDeleteModal}
+                    />
+                  </>
+                ) : (
+                  <>
+                    <ButtonComponent
+                      disabled={!isDirty || !isValid}
+                      isLoadingButton={true}
+                      isSubmitting={isSubmitting}
+                      label='Confirm'
+                      startIcon={icons.send}
+                    />
+                    <ButtonComponent
+                      disabled={!isDirty}
+                      isLoadingButton={false}
+                      label='Reset'
+                      onClick={() => reset()}
+                      startIcon={icons.reset}
+                    />
+                  </>
+                )}
                 <ButtonComponent
                   isLoadingButton={false}
-                  label='Delete'
-                  onClick={() => setDeleteModal(true)}
-                  startIcon={icons.delete}
+                  label='Cancel'
+                  onClick={() => setOpenModal(true)}
+                  startIcon={icons.cancel}
                 />
-                <ModalComponent
-                  modalTitle='Are you sure you want to delete the voucher?'
-                  modalDesc='Warning, all actions are irreversible.'
-                  // Note to myself: Need to change the line below to handle DELETE API
-                  clickHandler={() => setDeleteModal((prevState) => !prevState)}
-                  openModal={openDeleteModal}
-                  setOpenModal={setDeleteModal}
-                />
-              </>
-            ) : (
-              <>
-                <ButtonComponent
-                  disabled={!isDirty || !isValid}
-                  isLoadingButton={true}
-                  isSubmitting={isSubmitting}
-                  label='Confirm'
-                  startIcon={icons.send}
-                />
-                <ButtonComponent
-                  disabled={!isDirty}
-                  isLoadingButton={false}
-                  label='Reset'
-                  onClick={() => reset()}
-                  startIcon={icons.reset}
-                />
-              </>
-            )}
-            <ButtonComponent
-              isLoadingButton={false}
-              label='Cancel'
-              onClick={() => setOpenModal(true)}
-              startIcon={icons.cancel}
-            />
-          </ButtonGroup>
-          <ModalComponent
-            modalTitle='Are you sure you want to cancel?'
-            modalDesc='Warning, all changes are not saved upon clicking on Yes.'
-            clickHandler={() => navigate('/')}
-            openModal={openModal}
-            setOpenModal={setOpenModal}
-          />
-        </form>
+              </ButtonGroup>
+              <ModalComponent
+                modalTitle='Are you sure you want to cancel?'
+                modalDesc='Warning, all changes are not saved upon clicking on Yes.'
+                clickHandler={() => navigate('/')}
+                openModal={openModal}
+                setOpenModal={setOpenModal}
+              />
+            </form>
+          </div>
+        </motion.div>
       </Paper>
       <aside className='hidden w-[25%] rounded-lg xl:flex xl:w-3/6 xl:max-w-md xl:flex-col xl:items-center xl:justify-around'>
-        <h1 className='rounded-md bg-pink-400 py-4 px-6 text-center font-mont text-2xl text-gray-100 xl:text-3xl'>
+        <h1 className='cursor-default rounded-md bg-pink-400 py-4 px-6 text-center font-mont text-2xl text-gray-100 xl:text-3xl'>
           Voucher
         </h1>
         <Lottie className='max-w-[200px]' animationData={downArrow} />
