10aa86d1f563b1cb2cc62cb2e069333d3580db61
Nicholas <nicholas5538@gmail.com>
2023-03-23 13:07:46 +0800

    Fix lazy loading for forms (#37)

--- frontend-voucher/src/components/voucher-form/MemoVoucherForm.tsx
+++ frontend-voucher/src/components/voucher-form/MemoVoucherForm.tsx
@@ -3,7 +3,7 @@ import CheckCircleOutlineIcon from '@mui/icons-material/CheckCircleOutline';
 import ButtonGroup from '@mui/material/ButtonGroup';
 import Paper from '@mui/material/Paper';
 import clsx from 'clsx';
-import { motion, MotionConfig } from 'framer-motion';
+import { m, MotionConfig } from 'framer-motion';
 import Lottie, { type LottieRefCurrentProps } from 'lottie-react';
 import { memo, useEffect, useRef, useState } from 'react';
 import { SubmitHandler, useForm } from 'react-hook-form';
@@ -102,7 +102,7 @@ const VoucherFormComponent = ({ defaultValues }: VoucherFormProps) => {
             {watchAction} voucher
           </h2>
         </div>
-        <motion.div animate={{ height }} className='overflow-y-hidden'>
+        <m.div animate={{ height }} className='overflow-y-hidden'>
           <div ref={ref}>
             <AlertComponent
               className='mb-4 px-2'
@@ -255,7 +255,7 @@ const VoucherFormComponent = ({ defaultValues }: VoucherFormProps) => {
               />
             </form>
           </div>
-        </motion.div>
+        </m.div>
       </Paper>
       <aside className='hidden w-[25%] rounded-lg xl:flex xl:w-3/6 xl:max-w-md xl:flex-col xl:items-center xl:justify-around'>
         <h1 className='cursor-default rounded-md bg-pink-400 py-4 px-6 text-center font-mont text-2xl text-gray-100 xl:text-3xl'>
