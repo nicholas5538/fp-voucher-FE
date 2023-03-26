e1c39be3f375125f2ddfaceff68dda3e8ec4bb7b
Rui_Yang Chua <chuaruiyang@gmail.com>
2023-03-26 21:53:43 +0800

    Add Apply Voucher component modal

--- src/components/cart/voucher-modal.tsx
+++ src/components/cart/voucher-modal.tsx
@@ -1,17 +1,26 @@
 import {
   Box,
   Button,
+  Chip,
+  CircularProgress,
   Dialog,
   DialogActions,
-  DialogContentText,
+  DialogContent,
   DialogTitle,
   IconButton,
   Typography,
 } from '@mui/material';
-import { useState } from 'react';
+import { FC, useEffect, useState } from 'react';
 import styled from 'styled-components';
-import { ReactComponent as VoucherIcon } from '../../assets/voucher.svg';
+import VoucherIcon from '../../assets/voucher.svg';
 import { Close } from '@mui/icons-material';
+import PromoCodeField from './test';
+import { useForm, FormProvider } from 'react-hook-form';
+import { useQuery } from '@tanstack/react-query';
+import { getVouchers } from '../../utils/api';
+import VoucherCard from './cart-voucher-card';
+import { formatDate } from '../../utils/date';
+import { Dayjs } from 'dayjs';
 
 const StyledButton = styled(Button)`
   width: 100%;
@@ -19,8 +28,52 @@ const StyledButton = styled(Button)`
   box-shadow: none;
 `;
 
-const VoucherModal = () => {
-  const [open, setOpen] = useState(false);
+const StyledDialog = styled(Dialog)`
+  height: fit-content;
+  width: fit-content;
+  margin: auto;
+`;
+
+const StyledChip = styled(Chip)<{ $isActive: boolean }>`
+  margin-left: 0.8rem;
+  background-color: ${({ $isActive }) =>
+    $isActive ? '#e21b70' : 'transparent'};
+  color: ${({ $isActive }) => ($isActive ? 'white' : 'inherit')};
+  :hover {
+    color: white;
+    background-color: rgb(158 18 78) !important;
+  }
+`;
+
+type FormValues = {
+  promoCode?: string;
+};
+
+type VoucherModalProps = {
+  // eslint-disable-next-line no-unused-vars
+  onPromoCode: (code: string | undefined) => void;
+  promoCode?: string;
+};
+
+type VoucherType = {
+  id: string;
+  expiryDate: Dayjs;
+  promoCode: string;
+  discount: number;
+  description: string;
+};
+
+const VoucherModal: FC<VoucherModalProps> = ({ onPromoCode, promoCode }) => {
+  const methods = useForm<FormValues>();
+  const { handleSubmit, setValue, setError, resetField, watch } = methods;
+
+  const typedPromoCode = watch('promoCode');
+
+  const [sortMethod, setSortMethod] = useState<'discount' | 'expiryDate'>(
+    'discount',
+  );
+
+  const [open, setOpen] = useState(true);
 
   const handleClickOpen = () => {
     setOpen(true);
@@ -30,10 +83,75 @@ const VoucherModal = () => {
     setOpen(false);
   };
 
+  const [activeChip, setActiveChip] = useState<'discount' | 'expiryDate'>(
+    'discount',
+  );
+
+  useEffect(() => {
+    if (!promoCode) {
+      resetField('promoCode');
+      // setSelectedVoucher(null); // Reset the selected voucher state
+    } else {
+      setValue('promoCode', promoCode);
+    }
+  }, [promoCode, resetField, setValue]);
+
+  const { data, isLoading, isError } = useQuery({
+    queryKey: ['vouchers', { page: 0, pageSize: 20, signal: undefined }],
+    queryFn: () => getVouchers({ page: 0, pageSize: 20, signal: undefined }),
+  });
+
+  const pickUpVouchers = data?.vouchers.filter(
+    (voucher: { category: string }) => voucher.category === 'Pick-up',
+  );
+
+  const onSubmit = (data: FormValues) => {
+    const { promoCode } = data;
+    const isValid = pickUpVouchers?.find(
+      (voucher) => voucher.promoCode === promoCode,
+    );
+    if (!isValid) {
+      setError('promoCode', {
+        type: 'invalid',
+      });
+    } else {
+      onPromoCode(promoCode);
+      setOpen(false);
+    }
+  };
+
+  const handleSortMethodChange = (method: 'discount' | 'expiryDate') => {
+    setSortMethod(method);
+    setActiveChip(method);
+  };
+
+  const sortedPickUpVouchers = (vouchers: VoucherType[]) => {
+    if (sortMethod === 'discount') {
+      return vouchers.sort((a, b) => b.discount - a.discount);
+    } else if (sortMethod === 'expiryDate') {
+      return vouchers.sort(
+        (a, b) =>
+          new Date(
+            formatDate({
+              date: a.expiryDate,
+              dateFormat: 'YYYY-MM-DD',
+            }),
+          ).getTime() -
+          new Date(
+            formatDate({
+              date: b.expiryDate,
+              dateFormat: 'YYYY-MM-DD',
+            }),
+          ).getTime(),
+      );
+    }
+    return vouchers;
+  };
+
   return (
     <>
       <Box display='flex' marginY={6}>
-        <VoucherIcon />
+        <img src={VoucherIcon} alt='Voucher Icon' />
         <Typography
           sx={{ cursor: 'pointer' }}
           color='primary'
@@ -43,40 +161,103 @@ const VoucherModal = () => {
           Apply a voucher
         </Typography>
       </Box>
-      <Dialog
-        sx={{ padding: 32, maxWidth: '768px', marginX: 'auto' }}
-        open={open}
-        onClose={handleClose}
-      >
-        <DialogTitle>
-          <Box
-            display='flex'
-            justifyContent='space-between'
-            alignItems='center'
-          >
-            <Typography>Enter or select a voucher code</Typography>
-            <IconButton onClick={handleClose}>
-              <Close />
-            </IconButton>
-          </Box>
-        </DialogTitle>
-        <DialogContentText marginBottom={30}>
-          To subscribe to this website, please enter your email address here. We
-          will send updates occasionally.
-        </DialogContentText>
-        <DialogActions>
-          <StyledButton
-            variant='contained'
-            onClick={() => {
-              console.log('apply voucher');
-            }}
-          >
-            <Typography variant='body2' fontFamily=''>
-              Apply
-            </Typography>
-          </StyledButton>
-        </DialogActions>
-      </Dialog>
+      <FormProvider {...methods}>
+        <form>
+          <StyledDialog open={open} onClose={handleClose}>
+            <DialogTitle>
+              <Box
+                display='flex'
+                justifyContent='space-between'
+                alignItems='center'
+              >
+                <Typography>Enter or select a voucher code</Typography>
+                <IconButton onClick={handleClose}>
+                  <Close />
+                </IconButton>
+              </Box>
+            </DialogTitle>
+            <DialogContent sx={{ height: '30rem' }}>
+              <Box padding={4}>
+                <PromoCodeField />
+              </Box>
+              <Box
+                display='flex'
+                marginTop={2}
+                alignItems='center'
+                paddingBottom={6}
+              >
+                Sort By:
+                <StyledChip
+                  label='Discount'
+                  variant='outlined'
+                  $isActive={activeChip === 'discount'}
+                  onClick={() => handleSortMethodChange('discount')}
+                />
+                <StyledChip
+                  label='Expiry Date'
+                  variant='outlined'
+                  $isActive={activeChip === 'expiryDate'}
+                  onClick={() => handleSortMethodChange('expiryDate')}
+                />
+              </Box>
+              <Box
+                height='18rem'
+                overflow='auto'
+                paddingX={1}
+                paddingBottom={1}
+              >
+                {isError ? (
+                  <Box
+                    display='flex'
+                    justifyContent='center'
+                    alignItems='center'
+                    height='100%'
+                  >
+                    No vouchers available
+                  </Box>
+                ) : isLoading ? (
+                  <Box
+                    display='flex'
+                    justifyContent='center'
+                    alignItems='center'
+                    height='100%'
+                  >
+                    <CircularProgress color='primary' />
+                  </Box>
+                ) : (
+                  sortedPickUpVouchers(pickUpVouchers || []).map((voucher) => (
+                    <VoucherCard
+                      key={voucher.id}
+                      id={voucher.id}
+                      title={voucher.promoCode}
+                      description={voucher.description}
+                      expiryDate={formatDate({
+                        date: voucher.expiryDate,
+                        dateFormat: 'YYYY-MM-DD',
+                      })}
+                      discount={voucher.discount}
+                      onSelect={() => {
+                        setValue('promoCode', voucher.promoCode);
+                        watch('promoCode');
+                      }}
+                      isSelected={voucher.promoCode === typedPromoCode}
+                    />
+                  ))
+                )}
+              </Box>
+            </DialogContent>
+
+            <DialogActions>
+              <StyledButton
+                variant='contained'
+                onClick={handleSubmit(onSubmit)}
+              >
+                <Typography variant='body2'>Apply</Typography>
+              </StyledButton>
+            </DialogActions>
+          </StyledDialog>
+        </form>
+      </FormProvider>
     </>
   );
 };
