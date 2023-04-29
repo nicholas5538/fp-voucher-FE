a1c28421f96399bf43b088521a0f808f6eed67cc
nicholas5538 <nicholas5538@gmail.com>
2023-04-29 13:46:32 +0800

    Reduce chunk size on production

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,25 @@
-import { defineConfig } from 'vite';
+import { defineConfig, splitVendorChunkPlugin } from 'vite';
 import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), svgr()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: (id) => {
+          if (id.includes('@mui/x')) {
+            return 'vendor_mui_x';
+          } else if (id.includes('@mui')) {
+            return 'vendor_mui';
+          } else if (id.includes('lottie')) {
+            return 'vendor_lottie';
+          } else if (id.includes('framer')) {
+            return 'vendor_framer';
+          }
+        },
+      },
+    },
+  },
+  plugins: [react(), svgr(), splitVendorChunkPlugin()],
 });
