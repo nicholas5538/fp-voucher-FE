39009fc161a067b1b4b64a6616a45d845da3271d
nicholas5538 <nicholas5538@gmail.com>
2023-04-30 21:54:21 +0800

    Implement lazy loading and Suspense

--- src/pages/VoucherTable.tsx
+++ src/pages/VoucherTable.tsx
@@ -1,51 +1,18 @@
-/* eslint-disable @typescript-eslint/no-non-null-assertion */
 import TableViewTwoToneIcon from '@mui/icons-material/TableViewTwoTone';
-import { LinearProgress } from '@mui/material';
-import { DataGrid } from '@mui/x-data-grid';
-import { useQuery } from '@tanstack/react-query';
 import { motion } from 'framer-motion';
-import { useEffect, useState } from 'react';
-import { useSearchParams } from 'react-router-dom';
+import { Suspense } from 'react';
+import { useLoaderData } from 'react-router-dom';
 import useMeasure from 'react-use-measure';
 import AnimatedLayout from '../components/animated-layout';
-import CustomToolBar from '../components/table/CustomToolbar';
-import NoRows from '../components/table/NoRows';
+import { TPagination } from '../constants/globalTypes';
 import SkeletonTable from '../components/table/SkeletonTable';
-import tableColumns from '../components/table/table-columns';
+import DataTable from '../components/table/DataTable';
 import useTitle from '../hooks/useTitle';
-import { getVouchers } from '../utils/api';
 
 const VoucherTable = () => {
   useTitle('foodpanda Voucher Table');
   const [ref, { height }] = useMeasure();
-  const [searchParams, setSearchParams] = useSearchParams({
-    offset: '0',
-    limit: '10',
-  });
-  const [paginationModel, setPaginationModel] = useState({
-    page: 0,
-    pageSize: Number(searchParams.get('limit')),
-  });
-
-  const { data, isLoading, isPreviousData } = useQuery({
-    queryKey: ['vouchers', paginationModel],
-    queryFn: ({ signal }) => getVouchers({ ...paginationModel, signal }),
-    keepPreviousData: true,
-    refetchOnWindowFocus: false,
-    retry: 2,
-  });
-
-  const [rowCountState, setRowCountState] = useState<number>(data?.total || 0);
-
-  useEffect(() => {
-    setRowCountState((prevRowCountState) =>
-      data?.total !== undefined ? data?.total : prevRowCountState,
-    );
-    setSearchParams({
-      offset: `${paginationModel.page * paginationModel.pageSize}`,
-      limit: paginationModel.pageSize.toString(),
-    });
-  }, [data?.total, setRowCountState, paginationModel]);
+  const pageInfo = useLoaderData() as TPagination;
 
   return (
     <AnimatedLayout className='mx-auto mt-8 flex max-w-7xl flex-col items-start space-y-6 px-6'>
@@ -59,44 +26,9 @@ const VoucherTable = () => {
         className='w-full overflow-y-hidden'
       >
         <div ref={ref}>
-          {isLoading ? (
-            <SkeletonTable />
-          ) : (
-            <DataGrid
-              autoHeight
-              getRowHeight={() => 'auto'}
-              columns={tableColumns}
-              rows={data!.vouchers}
-              slots={{
-                loadingOverlay: LinearProgress,
-                noRowsOverlay: NoRows,
-                toolbar: CustomToolBar,
-              }}
-              slotProps={{
-                columnsPanel: {
-                  disableHideAllButton: true,
-                },
-              }}
-              sx={{
-                boxShadow: 4,
-                border: 2,
-                borderColor: 'hsl(334, 79%, 50%)',
-                fontFamily: 'Montserrat',
-                '& .MuiDataGrid-cell:hover': {
-                  color: 'hsl(334, 79%, 43%)',
-                  backgroundColor: 'transparent',
-                },
-                overflowX: 'scroll',
-              }}
-              disableRowSelectionOnClick={true}
-              rowCount={rowCountState}
-              loading={isPreviousData}
-              pageSizeOptions={[5, 10, 25, 50]}
-              paginationModel={paginationModel}
-              paginationMode='server'
-              onPaginationModelChange={setPaginationModel}
-            />
-          )}
+          <Suspense fallback={<SkeletonTable />}>
+            <DataTable {...pageInfo} />
+          </Suspense>
         </div>
       </motion.div>
     </AnimatedLayout>
