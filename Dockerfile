2ee82c8b96f44b4beea0dbee532f634f596e89e2
nicholas5538 <nicholas5538@gmail.com>
2023-08-28 18:16:00 +0800

    Add Dockerfile and dockerize app

---
+++ Dockerfile
@@ -0,0 +1,36 @@
+FROM node:current-alpine3.17 AS base
+LABEL authors="nicholas5538"
+LABEL version="1.0"
+
+COPY . /app
+WORKDIR /app
+
+# Install pnpm
+RUN yarn global add pnpm
+
+# Install dependencies
+FROM base AS dev-deps
+RUN --mount=type=cache,id=pnpm,target=/pnpm/store pnpm i --frozen-lockfile
+
+FROM base as prod-deps
+RUN npm pkg delete scripts.prepare
+RUN --mount=type=cache,id=pnpm,target=/pnpm/store pnpm i --prod --frozen-lockfile
+
+FROM base as build
+COPY --from=prod-deps ./app/node_modules ./node_modules
+RUN pnpm run build
+
+FROM base AS dev
+COPY --from=dev-deps ./app/node_modules ./node_modules
+
+EXPOSE 5173
+ENV PORT 5173
+
+CMD ["pnpm", "run", "dev"]
+
+FROM nginx:stable-alpine-slim
+COPY --from=build ./app/dist /usr/share/nginx/html
+COPY --from=build ./app/nginx.conf /etc/nginx/conf.d/default.conf
+
+EXPOSE 8080
+CMD ["nginx", "-g", "daemon off;"]
--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,5 @@
+import { resolve } from 'path';
+import { config } from 'dotenv';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import progress from 'vite-plugin-progress';
@@ -5,20 +7,41 @@ import svgr from 'vite-plugin-svgr';
 import TurboConsole from 'vite-plugin-turbo-console';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks: (id) => {
-          if (id.includes('@mui/base')) return 'vendor_mui_base';
-          else if (id.includes('@mui/material')) return 'vendor_mui_material';
-          else if (id.includes('@mui/system')) return 'vendor_mui_system';
-          else if (id.includes('@mui/x')) return 'vendor_mui_x';
-          else if (id.includes('lottie')) return 'vendor_lottie';
-          else if (id.includes('framer')) return 'vendor_framer';
+export default defineConfig(({ mode }) => {
+  const path = mode === 'development' ? '.env.development' : '.env.production';
+  config({ path });
+  return {
+    plugins: [react(), progress(), svgr(), TurboConsole()],
+    build: {
+      rollupOptions: {
+        output: {
+          manualChunks: (id) => {
+            if (id.includes('@mui/base')) return 'vendor_mui_base';
+            else if (id.includes('@mui/material')) return 'vendor_mui_material';
+            else if (id.includes('@mui/system')) return 'vendor_mui_system';
+            else if (id.includes('@mui/x')) return 'vendor_mui_x';
+            else if (id.includes('lottie')) return 'vendor_lottie';
+            else if (id.includes('framer')) return 'vendor_framer';
+          }
+        }
+      },
+      outDir: 'dist',
+      assetsDir: 'assets',
+      emptyOutDir: true,
+      target: 'esnext',
+    },
+    resolve: {
+      alias: {
+        '@': resolve(__dirname, './src'),
+      },
+    },
+    server: {
+      proxy: {
+        '/api': {
+          target: process.env.VITE_API_BASE_URL,
+          changeOrigin: true,
         },
       },
     },
-  },
-  plugins: [react(), progress(), svgr(), TurboConsole()],
+  }
 });
