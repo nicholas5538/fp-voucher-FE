de2e6fe26f6af945bd838fd1a9c1f6ffca55a0d4
Nicholas <nicholas8399@gmail.com>
2023-10-01 16:33:16 +0800

    Replace promise chains with async and await (#168)

--- src/hooks/useUserContext.tsx
+++ src/hooks/useUserContext.tsx
@@ -1,4 +1,4 @@
-import axios from 'axios';
+import axios, { type GenericAbortSignal } from 'axios';
 import { createContext, useContext, useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import type { childrenNode } from '../constants/globalTypes';
@@ -16,6 +16,25 @@ type TuserContext = {
   logout: () => void;
 };
 
+const getGoogleToken = async (
+  url: string,
+  accessToken: string,
+  signal: GenericAbortSignal | undefined,
+) =>
+  await axios.get(url, {
+    headers: {
+      Authorization: `Bearer ${accessToken}`,
+      Accept: 'application/json',
+    },
+    signal,
+  });
+
+const getJwt = async (url: string, email: string, name: string) =>
+  await axios.post(url, {
+    email,
+    name,
+  });
+
 const UserContext = createContext(null as unknown as TuserContext);
 
 export const useUserContext = () => useContext(UserContext);
@@ -27,49 +46,37 @@ const UserProvider = ({ children }: childrenNode) => {
   const navigate = useNavigate();
 
   const login = useGoogleLogin({
-    onSuccess: (tokenResponse) => {
-      const controller = new AbortController();
-      axios
-        .get(
-          'https://people.googleapis.com/v1/people/me?personFields=names,emailAddresses',
-          {
-            headers: {
-              Authorization: `Bearer ${tokenResponse.access_token}`,
-              Accept: 'application/json',
-            },
-            signal: controller.signal,
-          },
-        )
-        .then(async (res) => {
-          const { names, emailAddresses } = res.data;
-          setGivenName(names[0].givenName);
-          setEmail(emailAddresses[0].value);
-          setLocalStorageItem('name', names[0].givenName);
-          const expires = new Date(
-            new Date().getTime() + 7 * 24 * 60 * 60 * 1000,
-          );
-          const { data } = await axios.post(
-            'https://fp-capstone-backend.onrender.com/user',
-            {
-              email,
-              name: names[0].displayName,
-            },
-            {
-              headers: { 'Content-Type': 'application/json' },
-              signal: controller.signal,
-            },
-          );
-          return setCookie('jwt', data.token, {
-            expires,
-            path: '/',
-            secure: true,
-          });
-        })
-        .catch((err) => console.error(err));
-      return () => controller.abort();
+    onSuccess: async (tokenResponse) => {
+      const { abort, signal } = new AbortController();
+      const { data } = await getGoogleToken(
+        'https://people.googleapis.com/v1/people/me?personFields=names,emailAddresses',
+        tokenResponse.access_token,
+        signal,
+      );
+      const { names, emailAddresses } = data;
+
+      setGivenName(names[0].givenName);
+      setEmail(emailAddresses[0].value);
+      setLocalStorageItem('name', givenName);
+      setLocalStorageItem('token', tokenResponse.access_token);
+
+      // Generate JWT and set cookie
+      const { data: jwt } = await getJwt(
+        'https://fp-capstone-backend.onrender.com/user',
+        email,
+        givenName,
+      );
+      const expires = new Date(new Date().getTime() + 7 * 24 * 60 * 60 * 1000);
+
+      setCookie('jwt', jwt.token, {
+        expires,
+        path: '/',
+        secure: true,
+      });
+      return () => abort();
     },
     onError: (error) => {
-      console.error('Login Failed:', error);
+      throw new Error(`Login Failed: ${error.error_description}`);
     },
   });
   const logout = () => {
